c19e76e8b20d13de4994ef6870ee672a419faf72
u2lentaru <u2@lenta.ru>
2020-01-12 16:55:05 +0500

    Add files via upload

---
+++ sh/192.168.100.10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.31.1.1.1.6>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.31.1.1.1.10>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.2.2.1.10>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.2.2.1.16>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.31.1.1.1.18>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.2.2.1.2>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.2.2.1.8>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.100.10 .1.3.6.1.2.1.2.2.1.9>>/export/home/u2/MIBS/192.168.100.10/192.168.100.10_`/bin/date "+%Y%m%d%H%M"`
+
---
+++ sh/192.168.20.65.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.31.1.1.1.6>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.31.1.1.1.10>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.2.2.1.10>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.2.2.1.16>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.31.1.1.1.18>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.2.2.1.2>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.2.2.1.8>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
+/usr/local/bin/snmpbulkwalk -v 2c -c eut4read 192.168.20.65 .1.3.6.1.2.1.2.2.1.9>>/export/home/u2/MIBS/192.168.20.65/192.168.20.65_`/bin/date "+%Y%m%d%H%M"`
